b9be409ca5ed321fd01779782419679fc73f0c77
jafar09876 <jafar@airmasgroup.co.id>
2021-08-16 21:47:45 +0700

    fix leaderboard

--- app/Http/Livewire/Leaderboard/Index.php
+++ app/Http/Livewire/Leaderboard/Index.php
@@ -8,17 +8,18 @@ use App\ViewModels\VwLeadeboard;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class Index extends Component
+class Index extends
+    Component
 {
     use WithPagination;
 
     public $paginate = 10;
-    public $search, $filterByDepartment, $filterByGame, $totalData, $department;
+    public $search, $filterByDepartment, $totalData, $department;
     protected $updatesQueryString = ['search'];
 
     public function mount()
     {
-        $this->search = request()->query('search', $this->search);
+        $this->search     = request()->query('search', $this->search);
         $this->department = Department::all()->pluck('name', 'id');
     }
 
@@ -34,93 +35,43 @@ class Index extends Component
 
     public function updatedSearch($value)
     {
-        if ($value === "")
-        {
+        if ($value === "") {
             $this->search = null;
         }
     }
 
     public function updatedFilterByDepartment($value)
     {
-        $department = collect(User::select('department_id')->get())->pluck('department_id')->toArray();
+        $department = collect(Department::select('id')->get())->pluck('id')->toArray();
 
-        if ((!in_array($value, $department, true)))
-        {
+        if (!in_array($value, $department)) {
             $this->filterByDepartment = null;
-        }
-    }
-
-    public function updatedFilterByGame($value)
-    {
-        if ((!in_array($value, ["1", "2", "3", "4"], true)))
-        {
-            $this->filterByGame = null;
         }
     }
 
     public function render()
     {
-        if ($this->search !== null)
-        {
-            if ($this->filterByDepartment !== null and $this->filterByGame !== null)
-            {
-                $query           = VwLeadeboard::where('name', 'like',
-                    '%' . $this->search . '%')->where('department_id', $this->filterByDepartment)->where('quiz_ID',
-                    $this->filterByGame);
-                $this->totalData = $query->count();
-            } elseif ($this->filterByDepartment !== null)
-            {
-                $query           = VwLeadeboard::where('name', 'like', '%' . $this->search . '%')->where('name',
+        if ($this->search !== null) {
+            if ($this->filterByDepartment !== null) {
+                $query           = VwLeadeboard::where('name', 'like', '%'.$this->search.'%')->where('department_id',
                     $this->filterByDepartment);
                 $this->totalData = $query->count();
-            } elseif ($this->filterByGame !== null)
-            {
-                $query           = VwLeadeboard::where('name', 'like', '%' . $this->search . '%')->where('quiz_ID',
-                    $this->filterByGame);
-                $this->totalData = $query->count();
-            } else
-            {
-                $query           = VwLeadeboard::where('display_name', 'like', '%' . $this->search . '%');
+            } else {
+                $query           = VwLeadeboard::where('name', 'like', '%'.$this->search.'%');
                 $this->totalData = $query->count();
             }
-        } elseif ($this->filterByDepartment !== null)
-        {
-            if ($this->filterByGame !== null)
-            {
-                $query           = VwLeadeboard::where('division', $this->filterByDepartment)->where('quiz_ID',
-                    $this->filterByGame);
-                $this->totalData = $query->count();
-            } else
-            {
-                $query           = VwLeadeboard::where('division', $this->filterByDepartment);
-                $this->totalData = $query->count();
-            }
-        } elseif ($this->filterByGame !== null)
-        {
-            if ($this->filterByDepartment !== null)
-            {
-                $query           = VwLeadeboard::where('quiz_ID', $this->filterByGame)->where('division',
-                    $this->filterByDepartment);
-                $this->totalData = $query->count();
-
-            } else
-            {
-                $query           = VwLeadeboard::where('quiz_ID', $this->filterByGame);
-                $this->totalData = $query->count();
-            }
-        } else
-        {
+        } else {
             $this->totalData = VwLeadeboard::count();
-            
+
             return view('livewire.leaderboard.index', [
                 'department'   => $this->department,
-                'vwLeadeboard' => VwLeadeboard::with('department')->paginate($this->paginate)
+                'vwLeadeboard' => VwLeadeboard::with('department')->paginate($this->paginate),
             ]);
         }
-        
+
         return view('livewire.leaderboard.index', [
             'department'   => $this->department,
-            'vwLeadeboard' => $query->with('department')->paginate($this->paginate)
+            'vwLeadeboard' => $query->with('department')->paginate($this->paginate),
         ]);
     }
 }
