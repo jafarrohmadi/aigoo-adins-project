d089f3a1959404e570f7775fe708a9e4b9b52319
jafar09876 <jafar@airmasgroup.co.id>
2021-09-03 01:39:03 +0700

    guild score

--- app/Http/Resources/Leaderboard/LeaderBoardDataCollection.php
+++ app/Http/Resources/Leaderboard/LeaderBoardDataCollection.php
@@ -20,23 +20,42 @@ class LeaderBoardDataCollection extends
      */
     public function toArray($request)
     {
-        $date = $request->date ??date('Y-m');
-        $limit = $request->max_user  ?? 10;
-        $page = $request->page ?? 1;
+        $date  = $request->date ?? date('Y-m');
+        $limit = $request->max_user ?? 10;
+        $page  = $request->page ?? 1;
 
-        $userTotalScore = Cache::remember('userTotalScore'. me()->id. $date, 300, function () use ($date) {
-            return User::find(me()->id)->pointHistories->where('point', '>=', 0)->where('date_year_month' , $date)->sum('point');
+        $userTotalScore = Cache::remember('userTotalScore'.me()->id.$date, 300, function () use
+        (
+            $date
+        ) {
+            return User::find(me()->id)->pointHistories->where('point', '>=', 0)->where('date_year_month',
+                $date)->sum('point');
+        });
+
+        $userGuildScore = Cache::remember('userGuildScore'.me()->id.$date, 300, function () use
+        (
+            $date
+        ) {
+            return VwLeaderboardGuild::where('team_id', me()->department_id)->where('date', $date)->sum('total_points');
         });
 
 
-        $nationalData  = Cache::remember('nationalData'. me()->id. $date . $page. $limit, 300, function () use ($date, $limit) {
+        $nationalData = Cache::remember('nationalData'.me()->id.$date.$page.$limit, 300, function () use
+        (
+            $date,
+            $limit
+        ) {
             return VwLeadeboard::with('user', 'user.pointHistories')
                 ->where('date', $date)
                 ->orderBy('total_points', 'desc')
                 ->paginate($limit);
         });
 
-        $regionalData  = Cache::remember('regionalData'. me()->id. $date . $page.$limit, 300, function () use ($date, $limit) {
+        $regionalData = Cache::remember('regionalData'.me()->id.$date.$page.$limit, 300, function () use
+        (
+            $date,
+            $limit
+        ) {
             return VwLeadeboard::with('user', 'user.pointHistories')
                 ->where('date', $date)
                 ->where('team_id', me()->department_id)
@@ -44,32 +63,45 @@ class LeaderBoardDataCollection extends
                 ->paginate($limit);
         });
 
-        $guildData     = Cache::remember('guildData'. me()->id . $date , 300, function () use ($date) {
+        $guildData = Cache::remember('guildData'.me()->id.$date, 300, function () use
+        (
+            $date
+        ) {
             return VwLeaderboardGuild::with('department', 'department.leader', 'department.pointHistories')
-                ->where('date',$date)
+                ->where('date', $date)
                 ->orderBy('total_points', 'desc')
                 ->get();
         });
 
-        $ownPlaceRegional = Cache::remember('ownPlaceRegional'. me()->id, 300, function ()  use ($userTotalScore ,$date) {
-            return VwLeadeboard::with('user', 'user.pointHistories')
-                    ->where('team_id', me()->department_id)
-                    ->where('date',$date)
+        $ownPlaceRegional = Cache::remember('ownPlaceRegional'.me()->id, 300, function () use
+        (
+            $userTotalScore,
+            $date
+        ) {
+            return VwLeadeboard::where('team_id', me()->department_id)
+                    ->where('date', $date)
                     ->orderBy('total_points', 'desc')
                     ->where('total_points', '>', $userTotalScore)
                     ->count() + 1;
         });
 
-        $ownPlaceNational = Cache::remember('ownPlaceNational'. me()->id, 300, function ()  use ($userTotalScore, $date) {
-            return VwLeadeboard::with('user', 'user.pointHistories')
-                    ->where('total_points', '>', $userTotalScore)
-                    ->where('date' ,$date)
+        $ownPlaceNational = Cache::remember('ownPlaceNational'.me()->id, 300, function () use
+        (
+            $userTotalScore,
+            $date
+        ) {
+            return VwLeadeboard::where('total_points', '>', $userTotalScore)
+                    ->where('date', $date)
                     ->orderBy('total_points', 'desc')
                     ->count() + 1;
         });
 
-        $ownPlaceGuild = Cache::remember('ownPlaceGuild'. me()->id , 300, function () use ($guildData, $userTotalScore){
-           return count($guildData->where('total_points', '>', $userTotalScore)) + 1;
+        $ownPlaceGuild = Cache::remember('ownPlaceGuild'.me()->id, 300, function () use
+        (
+            $guildData,
+            $userTotalScore
+        ) {
+            return count($guildData->where('total_points', '>', $userTotalScore)) + 1;
         });
 
         return [
@@ -78,16 +110,19 @@ class LeaderBoardDataCollection extends
             'data'    => [
                 'regional' => [
                     'own_place'        => $ownPlaceRegional,
+                    'own_point'        => $userTotalScore,
                     'leaderboard_data' => LeaderBoardUserResource::collection($regionalData),
                 ],
 
                 'national' => [
                     'own_place'        => $ownPlaceNational,
+                    'own_point'        => $userTotalScore,
                     'leaderboard_data' => LeaderBoardUserResource::collection($nationalData),
                 ],
 
-                'guild'    => [
+                'guild' => [
                     'own_place'        => $ownPlaceGuild,
+                    'own_point'        => $userGuildScore,
                     'leaderboard_data' => LeaderBoardUserGuildResource::collection($guildData),
                 ],
             ],
